ef2883c41c2b8d5bceeb8efb9dfcba25b50a8dd0
Jef Driesen <jefdriesen@telenet.be>
2024-08-02 09:01:49 +0200

    Update to the new LVGL api
    In the new LVGL v9 api, the name of many symbols has changed, some
    structs are made opaque with helper functions to access their contents,
    some datatypes changed from 16 to 32bit, etc.

--- src/render/native/core/theme/theme.hpp
+++ src/render/native/core/theme/theme.hpp
@@ -4,8 +4,6 @@
 #include "lv_bindings_js.h"
 #include "native/core/style/font/font.hpp"
 
-static lv_theme_t theme_default;
-
-static bool theme_default_init = false;
+static lv_theme_t *theme_default = NULL;
 
 void NativeThemeInit (JSContext* ctx, JSValue& ns);
